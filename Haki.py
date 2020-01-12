f15dc37ae627c865deea8b902d372dbbc8ccb4b5
karieminnal <karieminnal@gmail.com>
2020-01-12 21:59:46 +0700

    update

--- Haki.py
+++ Haki.py
@@ -4,8 +4,17 @@ from time import sleep
 import os
 
 class EHaki(object):
-    def __init__(self):
+    def __init__(self, title, description, name, address, city, zip_code, province):
         self.driver = webdriver.Chrome()
+        self.title = title
+        self.description = description
+        self.name = name 
+        self.address = address
+        self.city = city
+        self.zip_code = zip_code
+        self.province = province
+        self.people = people
+        
     
     def web(self):
         self.driver.get('https://e-hakcipta.dgip.go.id/login')
@@ -18,7 +27,20 @@ class EHaki(object):
         
 
     def permohonan_baru(self):
-        self.driver.get('https://e-hakcipta.dgip.go.id/index.php/register/hakcipta')
+        print ("Masukan Nama : ")
+        self.name=input()
+        print("Masukan Alamat : ")
+        self.address=input()
+        print("Masukan Kota : ")
+        self.city=input()
+        print("Masukan Kode Pos : ")
+        self.zip_code=input()
+        print("masukan Provinsi")
+        self.province=input()
+        sleep(3)
+        self.driver.find_element_by_xpath('/html/body/div/div[1]/div/div/div[2]/div/div/ul/li[1]/a').click()
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div/div[1]/div/div/div[2]/div/div/ul/li[1]/ul/li[1]').click()
         sleep(2)
         self.driver.find_elements_by_class_name('close')[-1].click()
         sleep(2)
@@ -34,9 +56,9 @@ class EHaki(object):
         sleep(1)
         self.driver.find_elements_by_class_name('select2-results__option')[4].click()
         sleep(1)
-        self.driver.find_element_by_name('title').send_keys('Tutorial Penggunaan Kepo')
+        self.driver.find_element_by_name('title').send_keys(self.title)
         sleep(1)
-        self.driver.find_elements_by_name('description')[1].send_keys('Tutorial Penggunaan Kepo')
+        self.driver.find_elements_by_name('description')[1].send_keys(self.description)
         sleep(1)
         date = self.driver.find_element_by_xpath('//*[@id="createform"]/div[1]/div[2]/div/div[6]/div/div/input[1]')
 
@@ -51,24 +73,129 @@ class EHaki(object):
         self.driver.find_element_by_name('announced_city').send_keys("Bandung")
 
     def data_pencipta(self):
-        self.driver.get('https://e-hakcipta.dgip.go.id/index.php/register/hakcipta')
-        sleep(2)
-        self.driver.find_elements_by_class_name('close')[-1].click()
-        sleep(2)
+        # self.permohonan_baru()
+        # self.driver.get('https://e-hakcipta.dgip.go.id/index.php/register/hakcipta')
+        # sleep(2)
+        # self.driver.find_elements_by_class_name('close')[-1].click()
+        # sleep(2)
+
         self.driver.find_element_by_xpath('//*[@id="createform"]/div[3]/div[1]/div[2]/a').click()
         sleep(1)
-        self.driver.find_element_by_name("name").send_keys('innal')
+        self.driver.find_element_by_name("name").send_keys(self.name)
         sleep(1)
-        self.driver.find_element_by_xpath('//*[@id="creator"]/div[3]/div/textarea').send_keys('bandung')
+        self.driver.find_element_by_xpath('//*[@id="creator"]/div[3]/div/textarea').send_keys(self.address)
         sleep(1)
-        self.driver.find_element_by_name('city').send_keys('bandung')
+        self.driver.find_element_by_name('city').send_keys(self.city)
         sleep(1)
-        self.driver.find_element_by_name('zip_code').send_keys('404401')
+        self.driver.find_element_by_name('zip_code').send_keys(self.zip_code)
         sleep(1)
+        
         self.driver.find_element_by_name('province').click()
         sleep(1)
-        self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[11]').click()
-        sleep(1)
+        if self.province == "Jawa barat" :
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[11]').click()
+            sleep(1)
+        elif self.province == "Bali":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[2]').click()
+            sleep(1)
+        elif self.province == "Bangka Belitung":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[3]').click()
+            sleep(1)
+        elif self.province == "Banten":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[4]').click()
+            sleep(1)
+        elif self.province == "Bengkulu":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[5]').click()
+            sleep(1)
+        elif self.province == "Di Aceh":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[6]').click()
+            sleep(1)
+        elif self.province == "Di Yogyakarta":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[7]').click()
+            sleep(1)
+        elif self.province == "DKI Jakarta":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[8]').click()
+            sleep(1)
+        elif self.province == "Gorontalo":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[9]').click()
+            sleep(1)
+        elif self.province == "Jambi":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[10]').click()
+            sleep(1)
+        elif self.province == "Jawa Tengah":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[12]').click()
+            sleep(1)
+        elif self.province == "Jawa Timur":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[13]').click()
+            sleep(1)
+        elif self.province == "Kalimantan Barat":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[14]').click()
+            sleep(1)
+        elif self.province == "Kalimantan Selatan":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[15]').click()
+            sleep(1)
+        elif self.province == "Kalimantan Tengah":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[16]').click()
+            sleep(1)
+        elif self.province == "kalimantan Timur":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[17]').click()
+            sleep(1)
+        elif self.province == "Kalimantan Utara":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[18]').click()
+            sleep(1)
+        elif self.province == "Kepulauan Riau":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[19]').click()
+            sleep(1)
+        elif self.province == "Lampung":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[20]').click()
+            sleep(1)
+        elif self.province == "Maluku":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[21]').click()
+            sleep(1)
+        elif self.province == "Maluku Utara":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[22]').click()
+            sleep(2)
+        elif self.province == "Nusa Tenggara Barat":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[23]').click()
+            sleep(1)
+        elif self.province == "Nusa Tenggara Timur":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[24]').click()
+            sleep(1)
+        elif self.province == "Papua":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[25]').click()
+            sleep(1)
+        elif self.province == "Papua Barat":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[26]').click()
+            sleep(1)
+        elif self.province == "Riau":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[27]').click()
+            sleep(1)
+        elif self.province == "Sulawesi Barat":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[28]').click()
+            sleep(1)
+        elif self.province == "Sulawesi Selatan":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[29]').click()
+            sleep(1)
+        elif self.province == "Sulawesi Tengah":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[30]').click()
+            sleep(1)
+        elif self.province == "Sulawesi Tenggara":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[31]').click()
+            sleep(1)    
+        elif self.province == "Sulawesi Utara":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[32]').click()
+            sleep(1)
+        elif self.province == "Sumatera Barat":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[33]').click()
+            sleep(1)
+        elif self.province == "Sumatera Selatan":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[34]').click()
+            sleep(1)
+        elif self.province == "Sumatera Utara":
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[35]').click()
+            sleep(1)
+        else :
+            print("Kota tidak ditemukan !!!!")
         self.driver.find_element_by_xpath('//*[@id="creator"]/div[9]/input').click()
 
     def pemegang_hak_cipta(self):
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import Haki
 
-jalan=Haki.EHaki()
+
+print("Untuk Berapa Orang ? ")
+people=input()
+for x in people :
+
+print ("Masukan Judul : ")
+title=input()
+print ("Masukan deskripsi : ")
+decription=input()
+print ("Masukan Nama : ")
+name=input()
+print("Masukan Alamat : ")
+address=input()
+print("Masukan Kota : ")
+city=input()
+print("Masukan Kode Pos : ")
+zip_code=input()
+print("masukan Provinsi")
+province=input()
+jalan=Haki.EHaki(title, decription, name, address, city, zip_code, province)
 jalan.web()
-jalan.lampiran()
+jalan.permohonan_baru()
+jalan.data_pencipta()
