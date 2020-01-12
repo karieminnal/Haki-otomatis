b24e2fe5c2c453c59a0559b3d1e980e9e9d63862
karieminnal <karieminnal@gmail.com>
2020-01-12 22:54:45 +0700

    berhasil boy

--- Haki.py
+++ Haki.py
@@ -4,7 +4,7 @@ from time import sleep
 import os
 
 class EHaki(object):
-    def __init__(self, title, description, date, name, address, city, zip_code, province):
+    def __init__(self, title, description, date, name, address, city, zip_code, province, people):
         self.driver = webdriver.Chrome()
         self.title = title
         self.description = description
@@ -14,6 +14,7 @@ class EHaki(object):
         self.city = city
         self.zip_code = zip_code
         self.province = province
+        self.people = people
         
     
     def web(self):
@@ -67,126 +68,127 @@ class EHaki(object):
         # sleep(2)
         # self.driver.find_elements_by_class_name('close')[-1].click()
         # sleep(2)
+        for x in range (self.people):
 
-        self.driver.find_element_by_xpath('//*[@id="createform"]/div[3]/div[1]/div[2]/a').click()
-        sleep(1)
-        self.driver.find_element_by_name("name").send_keys(self.name)
-        sleep(1)
-        self.driver.find_element_by_xpath('//*[@id="creator"]/div[3]/div/textarea').send_keys(self.address)
-        sleep(1)
-        self.driver.find_element_by_name('city').send_keys(self.city)
-        sleep(1)
-        self.driver.find_element_by_name('zip_code').send_keys(self.zip_code)
-        sleep(1)
-        
-        self.driver.find_element_by_name('province').click()
-        sleep(1)
-        if self.province == "jawa barat" :
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[11]').click()
+            self.driver.find_element_by_xpath('//*[@id="createform"]/div[3]/div[1]/div[2]/a').click()
             sleep(1)
-        elif self.province == "bali":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[2]').click()
+            self.driver.find_element_by_name("name").send_keys(self.name[x])
             sleep(1)
-        elif self.province == "bangka belitung":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[3]').click()
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[3]/div/textarea').send_keys(self.address[x])
             sleep(1)
-        elif self.province == "banten":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[4]').click()
+            self.driver.find_element_by_name('city').send_keys(self.city[x])
             sleep(1)
-        elif self.province == "bengkulu":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[5]').click()
+            self.driver.find_element_by_name('zip_code').send_keys(self.zip_code[x])
             sleep(1)
-        elif self.province == "di aceh":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[6]').click()
+            
+            self.driver.find_element_by_name('province').click()
             sleep(1)
-        elif self.province == "di yogyakarta":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[7]').click()
-            sleep(1)
-        elif self.province == "dki jakarta":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[8]').click()
-            sleep(1)
-        elif self.province == "gorontalo":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[9]').click()
-            sleep(1)
-        elif self.province == "jambi":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[10]').click()
-            sleep(1)
-        elif self.province == "jawa tengah":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[12]').click()
-            sleep(1)
-        elif self.province == "jawa timur":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[13]').click()
-            sleep(1)
-        elif self.province == "kalimantan barat":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[14]').click()
-            sleep(1)
-        elif self.province == "kalimantan selatan":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[15]').click()
-            sleep(1)
-        elif self.province == "kalimantan tengah":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[16]').click()
-            sleep(1)
-        elif self.province == "kalimantan timur":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[17]').click()
-            sleep(1)
-        elif self.province == "kalimantan utara":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[18]').click()
-            sleep(1)
-        elif self.province == "kepulauan riau":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[19]').click()
-            sleep(1)
-        elif self.province == "lampung":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[20]').click()
-            sleep(1)
-        elif self.province == "maluku":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[21]').click()
-            sleep(1)
-        elif self.province == "maluku utara":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[22]').click()
-            sleep(2)
-        elif self.province == "nusa tenggara barat":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[23]').click()
-            sleep(1)
-        elif self.province == "nusa tenggara timur":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[24]').click()
-            sleep(1)
-        elif self.province == "papua":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[25]').click()
-            sleep(1)
-        elif self.province == "papua barat":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[26]').click()
-            sleep(1)
-        elif self.province == "riau":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[27]').click()
-            sleep(1)
-        elif self.province == "sulawesi barat":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[28]').click()
-            sleep(1)
-        elif self.province == "sulawesi selatan":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[29]').click()
-            sleep(1)
-        elif self.province == "sulawesi tengah":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[30]').click()
-            sleep(1)
-        elif self.province == "sulawesi tenggara":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[31]').click()
-            sleep(1)    
-        elif self.province == "sulawesi utara":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[32]').click()
-            sleep(1)
-        elif self.province == "sumatera barat":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[33]').click()
-            sleep(1)
-        elif self.province == "sumatera selatan":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[34]').click()
-            sleep(1)
-        elif self.province == "sumatera utara":
-            self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[35]').click()
-            sleep(1)
-        else :
-            print("Kota tidak ditemukan !!!!")
-        self.driver.find_element_by_xpath('//*[@id="creator"]/div[9]/input').click()
-        sleep(5)
+            if self.province[x] == "jawa barat" :
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[11]').click()
+                sleep(1)
+            elif self.province[x] == "bali":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[2]').click()
+                sleep(1)
+            elif self.province[x] == "bangka belitung":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[3]').click()
+                sleep(1)
+            elif self.province[x] == "banten":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[4]').click()
+                sleep(1)
+            elif self.province[x] == "bengkulu":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[5]').click()
+                sleep(1)
+            elif self.province[x] == "di aceh":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[6]').click()
+                sleep(1)
+            elif self.province[x] == "di yogyakarta":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[7]').click()
+                sleep(1)
+            elif self.province[x] == "dki jakarta":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[8]').click()
+                sleep(1)
+            elif self.province[x] == "gorontalo":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[9]').click()
+                sleep(1)
+            elif self.province[x] == "jambi":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[10]').click()
+                sleep(1)
+            elif self.province[x] == "jawa tengah":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[12]').click()
+                sleep(1)
+            elif self.province[x] == "jawa timur":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[13]').click()
+                sleep(1)
+            elif self.province[x] == "kalimantan barat":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[14]').click()
+                sleep(1)
+            elif self.province[x] == "kalimantan selatan":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[15]').click()
+                sleep(1)
+            elif self.province[x] == "kalimantan tengah":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[16]').click()
+                sleep(1)
+            elif self.province[x] == "kalimantan timur":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[17]').click()
+                sleep(1)
+            elif self.province[x] == "kalimantan utara":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[18]').click()
+                sleep(1)
+            elif self.province[x] == "kepulauan riau":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[19]').click()
+                sleep(1)
+            elif self.province[x] == "lampung":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[20]').click()
+                sleep(1)
+            elif self.province[x] == "maluku":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[21]').click()
+                sleep(1)
+            elif self.province[x] == "maluku utara":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[22]').click()
+                sleep(2)
+            elif self.province[x] == "nusa tenggara barat":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[23]').click()
+                sleep(1)
+            elif self.province[x] == "nusa tenggara timur":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[24]').click()
+                sleep(1)
+            elif self.province[x] == "papua":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[25]').click()
+                sleep(1)
+            elif self.province[x] == "papua barat":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[26]').click()
+                sleep(1)
+            elif self.province[x] == "riau":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[27]').click()
+                sleep(1)
+            elif self.province[x] == "sulawesi barat":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[28]').click()
+                sleep(1)
+            elif self.province[x] == "sulawesi selatan":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[29]').click()
+                sleep(1)
+            elif self.province[x] == "sulawesi tengah":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[30]').click()
+                sleep(1)
+            elif self.province[x] == "sulawesi tenggara":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[31]').click()
+                sleep(1)    
+            elif self.province[x] == "sulawesi utara":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[32]').click()
+                sleep(1)
+            elif self.province[x] == "sumatera barat":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[33]').click()
+                sleep(1)
+            elif self.province[x] == "sumatera selatan":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[34]').click()
+                sleep(1)
+            elif self.province[x] == "sumatera utara":
+                self.driver.find_element_by_xpath('//*[@id="creator"]/div[8]/div/select/option[35]').click()
+                sleep(1)
+            else :
+                print("Kota tidak ditemukan !!!!")
+            self.driver.find_element_by_xpath('//*[@id="creator"]/div[9]/input').click()
+            sleep(5)
 
     def pemegang_hak_cipta(self):
         self.driver.find_element_by_xpath('//*[@id="createform"]/div[4]/div[1]/div[2]/a').click()
